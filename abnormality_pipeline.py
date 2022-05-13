0c82656c6df21e83dd53db967d77d00a4c90057d
Rohit Lokwani <rohitlokwani17@gmail.com>
2022-05-13 01:36:32 -0700

    Add files via upload

---
+++ abnormality_pipeline.py
@@ -0,0 +1,275 @@
+import numpy as np 
+import keras
+from keras.layers import Conv2D,MaxPooling2D,Dropout,LeakyReLU,concatenate,ZeroPadding2D,BatchNormalization,Conv2DTranspose
+from keras.layers import Add
+from keras.models import Model
+from keras.layers import Input, Dense
+import os 
+import cv2 
+from keras.applications.densenet import DenseNet121
+from matplotlib.pyplot import imshow,title ,show
+from keras.applications import Xception
+from keras import backend as K
+from keras.losses import binary_crossentropy as binary_crossentropy
+from keras.callbacks import ModelCheckpoint,Callback
+from keras.optimizers import Adam
+from matplotlib import pyplot as plt
+
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, Reshape, Dense, multiply, Permute, Concatenate, Conv2D, Add, Activation, Lambda
+from keras import backend as K
+from keras.activations import sigmoid
+
+
+
+
+def attach_attention_module(net, attention_module):
+    if attention_module == 'se_block': # SE_block
+        net = se_block(net)
+    elif attention_module == 'cbam_block': # CBAM_block
+        net = cbam_block(net)
+    else:
+        raise Exception("'{}' is not supported attention module!".format(attention_module))
+
+    return net
+
+def se_block(input_feature, ratio=8):
+    """Contains the implementation of Squeeze-and-Excitation(SE) block.
+    As described in https://arxiv.org/abs/1709.01507.
+    """
+    
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    channel = input_feature._keras_shape[channel_axis]
+
+    se_feature = GlobalAveragePooling2D()(input_feature)
+    se_feature = Reshape((1, 1, channel))(se_feature)
+    assert se_feature._keras_shape[1:] == (1,1,channel)
+    se_feature = Dense(channel // ratio,
+                       activation='relu',
+                       kernel_initializer='he_normal',
+                       use_bias=True,
+                       bias_initializer='zeros')(se_feature)
+    assert se_feature._keras_shape[1:] == (1,1,channel//ratio)
+    se_feature = Dense(channel,
+                       activation='sigmoid',
+                       kernel_initializer='he_normal',
+                       use_bias=True,
+                       bias_initializer='zeros')(se_feature)
+    assert se_feature._keras_shape[1:] == (1,1,channel)
+    if K.image_data_format() == 'channels_first':
+        se_feature = Permute((3, 1, 2))(se_feature)
+
+    se_feature = multiply([input_feature, se_feature])
+    return se_feature
+
+def cbam_block(cbam_feature, ratio=8):
+    """Contains the implementation of Convolutional Block Attention Module(CBAM) block.
+    As described in https://arxiv.org/abs/1807.06521.
+    """
+    
+    cbam_feature = channel_attention(cbam_feature, ratio)
+    cbam_feature = spatial_attention(cbam_feature)
+    return cbam_feature
+
+def channel_attention(input_feature, ratio=8):
+    
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    channel = input_feature._keras_shape[channel_axis]
+    
+    shared_layer_one = Dense(channel//ratio,
+                             activation='relu',
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+    shared_layer_two = Dense(channel,
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+    
+    avg_pool = GlobalAveragePooling2D()(input_feature)    
+    avg_pool = Reshape((1,1,channel))(avg_pool)
+    assert avg_pool._keras_shape[1:] == (1,1,channel)
+    avg_pool = shared_layer_one(avg_pool)
+    assert avg_pool._keras_shape[1:] == (1,1,channel//ratio)
+    avg_pool = shared_layer_two(avg_pool)
+    assert avg_pool._keras_shape[1:] == (1,1,channel)
+    
+    max_pool = GlobalMaxPooling2D()(input_feature)
+    max_pool = Reshape((1,1,channel))(max_pool)
+    assert max_pool._keras_shape[1:] == (1,1,channel)
+    max_pool = shared_layer_one(max_pool)
+    assert max_pool._keras_shape[1:] == (1,1,channel//ratio)
+    max_pool = shared_layer_two(max_pool)
+    assert max_pool._keras_shape[1:] == (1,1,channel)
+    
+    cbam_feature = Add()([avg_pool,max_pool])
+    cbam_feature = Activation('sigmoid')(cbam_feature)
+    
+    if K.image_data_format() == "channels_first":
+        cbam_feature = Permute((3, 1, 2))(cbam_feature)
+    
+    return multiply([input_feature, cbam_feature])
+
+def spatial_attention(input_feature):
+    kernel_size = 7
+    
+    if K.image_data_format() == "channels_first":
+        channel = input_feature._keras_shape[1]
+        cbam_feature = Permute((2,3,1))(input_feature)
+    else:
+        channel = input_feature._keras_shape[-1]
+        cbam_feature = input_feature
+    
+    avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+    assert avg_pool._keras_shape[-1] == 1
+    max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+    assert max_pool._keras_shape[-1] == 1
+    concat = Concatenate(axis=3)([avg_pool, max_pool])
+    assert concat._keras_shape[-1] == 2
+    cbam_feature = Conv2D(filters = 1,
+                    kernel_size=kernel_size,
+                    strides=1,
+                    padding='same',
+                    activation='sigmoid',
+                    kernel_initializer='he_normal',
+                    use_bias=False)(concat) 
+    assert cbam_feature._keras_shape[-1] == 1
+    
+    if K.image_data_format() == "channels_first":
+        cbam_feature = Permute((3, 1, 2))(cbam_feature)
+        
+    return multiply([input_feature, cbam_feature])
+
+
+
+def convolution_block(x, filters, size, strides=(1,1), padding='same', activation=True):
+    x = Conv2D(filters, size, strides=strides, padding=padding)(x)
+    x = BatchNormalization()(x)
+    #x = attach_attention_module(x,attention_module='cbam_block')
+    if activation == True:
+        x = LeakyReLU(alpha=0.1)(x)
+    return x
+
+def residual_block(blockInput, num_filters=16):
+    x = LeakyReLU(alpha=0.1)(blockInput)
+    x = BatchNormalization()(x)
+    x = attach_attention_module(x,attention_module='cbam_block')
+    blockInput = BatchNormalization()(blockInput)
+    x = convolution_block(x, num_filters, (3,3) )
+    x = convolution_block(x, num_filters, (3,3), activation=False)
+    x = Add()([x, blockInput])
+    #x = attach_attention_module(x,attention_module='cbam_block')
+    return x
+
+def UXception(input_shape=(None, None, 3),dropout_rate=0.5):
+
+    backbone = Xception(input_shape=input_shape,weights='imagenet',include_top=False)
+    input = backbone.input
+    start_neurons = 16
+
+    conv4 = backbone.layers[121].output
+    conv4 = LeakyReLU(alpha=0.1)(conv4)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(dropout_rate)(pool4)
+    
+     # Middle
+    convm = Conv2D(start_neurons * 32, (3, 3), activation=None, padding="same")(pool4)
+    convm = residual_block(convm,start_neurons * 32)
+    convm = residual_block(convm,start_neurons * 32)
+    convm = LeakyReLU(alpha=0.1)(convm)
+    
+    deconv4 = Conv2DTranspose(start_neurons * 16, (3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(dropout_rate)(uconv4)
+    
+    uconv4 = Conv2D(start_neurons * 16, (3, 3), activation=None, padding="same")(uconv4)
+    uconv4 = residual_block(uconv4,start_neurons * 16)
+    uconv4 = residual_block(uconv4,start_neurons * 16)
+    uconv4 = LeakyReLU(alpha=0.1)(uconv4)
+    
+    deconv3 = Conv2DTranspose(start_neurons * 8, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    conv3 = backbone.layers[31].output
+    uconv3 = concatenate([deconv3, conv3])    
+    uconv3 = Dropout(dropout_rate)(uconv3)
+    
+    uconv3 = Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(uconv3)
+    uconv3 = residual_block(uconv3,start_neurons * 8)
+    uconv3 = residual_block(uconv3,start_neurons * 8)
+    uconv3 = LeakyReLU(alpha=0.1)(uconv3)
+
+    deconv2 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv3)
+    conv2 = backbone.layers[21].output
+    conv2 = ZeroPadding2D(((1,0),(1,0)))(conv2)
+    uconv2 = concatenate([deconv2, conv2])
+        
+    uconv2 = Dropout(0.1)(uconv2)
+    uconv2 = Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(uconv2)
+    uconv2 = residual_block(uconv2,start_neurons * 4)
+    uconv2 = residual_block(uconv2,start_neurons * 4)
+    uconv2 = LeakyReLU(alpha=0.1)(uconv2)
+    
+    deconv1 = Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same")(uconv2)
+    conv1 = backbone.layers[11].output
+    conv1 = ZeroPadding2D(((3,0),(3,0)))(conv1)
+    uconv1 = concatenate([deconv1, conv1])
+    
+    uconv1 = Dropout(0.1)(uconv1)
+    uconv1 = Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(uconv1)
+    uconv1 = residual_block(uconv1,start_neurons * 2)
+    uconv1 = residual_block(uconv1,start_neurons * 2)
+    uconv1 = LeakyReLU(alpha=0.1)(uconv1)
+    
+    uconv0 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv1)   
+    uconv0 = Dropout(dropout_rate)(uconv0)
+    uconv0 = Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(uconv0)
+    uconv0 = residual_block(uconv0,start_neurons * 1)
+    uconv0 = residual_block(uconv0,start_neurons * 1)
+    uconv0 = LeakyReLU(alpha=0.1)(uconv0)
+    
+    uconv0 = Dropout(dropout_rate/2)(uconv0)
+    output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv0)    
+    
+    model = Model(input, output_layer)
+    model.name = 'u-xception'
+
+    return model
+
+
+# Actual Function Calls Begin here
+
+img_size = 512
+
+def abnormality_model():
+    seg_model = UXception(input_shape=(img_size,img_size,3))
+    saved_weights_pth_unet = 'x.hdf5'
+
+    seg_model.load_weights(saved_weights_pth_unet)
+    
+    return seg_model
+
+def preprocessing(x):
+    image2=cv2.resize(x, (img_size,img_size))
+    image=np.expand_dims(image2.astype('float')/255.0,axis=0)
+    return image
+
+def postprocessing(predicted_mask):
+    original_threshold=0.1
+    
+    if np.max(predicted_mask[0,:,:,0])>original_threshold:
+        predicted_class=1
+        
+#         predicted_mask=cv2.resize(predicted_mask, (img_size,img_size))
+        mask_test = np.reshape(predicted_mask[0,:,:,0],(img_size,img_size))
+        mask_test=np.where(mask_test > original_threshold,1,0)
+        mask_test = (mask_test*255.0).astype('uint8')
+        
+    else:
+        predicted_class=0
+        mask_test=np.zeros(predicted_mask[0].shape, dtype='uint8')
+        mask_test = np.reshape(mask_test,(img_size,img_size))
+        mask_test[mask_test<original_threshold] = 0
+        
+    return (original_threshold, np.max(predicted_mask[0,:,:,0]), predicted_class, mask_test)
+
+def inference(x):
+    predicted_mask=seg_model.predict(preprocessing(x),verbose=0)
+    original_threshold, predicted_probability, predicted_class, predicted_mask = postprocessing(predicted_mask[0])
